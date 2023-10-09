c29609103eaf8891a92beffb6477975870fd131d
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-10-09 12:13:20 -0300

    router new page

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photo":"link",
+    "title": "UM BONITO TÍTULO AQUI",
+    "description": "Uma bonita descrição aqui"
+  },
+  {
+    "id":2,
+    "photo":"link",
+    "title": "UM BONITO TÍTULO AQUI",
+    "description": "Uma bonita descrição aqui"
+  }
+];
---
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css'],
+})
+export class ContentComponent {
+  photoCover: string = '';
+  contentTitle: string = '';
+  contentDescription: string = '';
+
+  constructor() {}
+}
