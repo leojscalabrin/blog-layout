a177eb079a71175d770ce5f0ee1d8e09efcad9fb
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-10-09 12:42:27 -0300

    content page dinamica

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"link",
+    "id":"1",
     "title": "UM BONITO TÍTULO AQUI",
-    "description": "Uma bonita descrição aqui"
+    "description": "Uma bonita descrição aqui",
+    "photoCover": "https://i.stack.imgur.com/y9DpT.jpg"
   },
   {
-    "id":2,
-    "photo":"link",
-    "title": "UM BONITO TÍTULO AQUI",
-    "description": "Uma bonita descrição aqui"
+    "id":"2",
+    "title": "OUTRO BONITO TÍTULO AQUI",
+    "description": "Outra bonita descrição aqui",
+    "photoCover": "https://i.stack.imgur.com/y9DpT.jpg"
   }
 ];
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { dataFake } from '../../data/dataFake';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
@@ -10,12 +10,23 @@ export class ContentComponent implements OnInit {
   photoCover: string = '';
   contentTitle: string = '';
   contentDescription: string = '';
+  private id:string | null = '0';
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe( value =>
-      console.log(value.get("id"))
+     this.id = value.get("id")
     )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter((article) => article.id === id)[0];
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
   }
 }
